7d4ed01fbc928cf0915b7a5efd684f561274273d
salsal <to.salsal.pro@gmail.com>
2026-02-23 06:42:04 -0500

    basic things

---
+++ app/components/modules/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="parent mx-12 px-3 py-2 "> 
+        <div className="text-white">
+          HeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeader
+        </div>
+      </div>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono , Vazirmatn } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const vazirmatnsans = Vazirmatn({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${vazirmatnsans.variable} antialiased bg-[#f7f9fb]`}
       >
         {children}
       </body>
