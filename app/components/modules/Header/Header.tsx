1fad5284341df5f5c3d5c2570df4752bdb6fe05d
salsal <to.salsal.pro@gmail.com>
2026-05-30 12:16:32 -0400

    fixing header

--- app/components/modules/Header/Header.tsx
+++ app/components/modules/Header/Header.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaPhoneAlt } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 interface NavItems {
   id: number;
@@ -18,55 +19,75 @@ const MenuItems: NavItems[] = [
 ];
 
 export default function Header() {
-  const [isFixed, setIsFixed] = useState<boolean>(false);
-
-  const handleScroll = () => {
-    if (window.scrollY > 20) {
-      setIsFixed(true);
-      console.log("12");
-    } else {
-      setIsFixed(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const [IsShowMenu, setIsShowMenu] = useState<boolean>(false);
 
   return (
-    <header className="parent w-full py-6">
-      <div className={`theNavBox shadowbx border px-3 py-2 mx-6 rounded-2xl ${isFixed ? 'retr' : 'etgt'}`}>
-        <div className="m-auto desktop-header-none-scroling grid grid-cols-4 px-3 my-5 rounded-2xl">
-          <div className="col-span-1  items-center">
-            <div className="phone-number w-full h-full flex justify-center gap-3 items-center">
-              <div className="svg text-color">
-                <FaPhoneAlt size={14} />
+    <header className=" w-full py-6">
+      <div className="desktop-header hidden lg:block ">
+        <div className="h-22.5 block"></div>
+        <div
+          className={`theNavBox shadowbx border px-3 py-2 mx-6 rounded-2xl fixed top-6 right-0 left-0`}
+        >
+          <div className="m-auto grid grid-cols-4 px-3 my-5">
+            <div className="col-span-1  items-center">
+              <div className="phone-number w-full h-full flex justify-center gap-3 items-center">
+                <div className="svg text-color">
+                  <FaPhoneAlt size={14} />
+                </div>
+                <div className="number text-color">09379424323</div>
               </div>
-              <div className="number text-color">09379424323</div>
             </div>
-          </div>
-          <div className="col-span-2">
-            <ul className="w-full h-full flex justify-around items-center">
-              {MenuItems.map((e) => (
-                <Link href={e.href} key={e.id}>
-                  <li className="links_ text-gray-500 text-xl">{e.label}</li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-          <div className="col-span-1 ">
-            <div className="logo w-full h-full flex justify-center items-center">
-              <span className="text-black font-bold text-2xl">salsal</span>
+            <div className="col-span-2">
+              <ul className="w-full h-full flex justify-around items-center">
+                {MenuItems.map((e) => (
+                  <Link href={e.href} key={e.id}>
+                    <li className="links_ text-gray-500 text-xl">{e.label}</li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+            <div className="col-span-1 ">
+              <div className="logo w-full h-full flex justify-center items-center">
+                <span className="text-black font-bold text-2xl">salsal</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="w-7xl mx-auto desktop-header grid grid-cols-4 px-3 bg-white "></div>
+      <div className="mobile-header block lg:hidden">
+        <div className="h-22.5 block"></div>
+        <div className="theNavBox shadowbx border px-3 mx-6 rounded-xl fixed top-6 right-0 left-0">
+          <div className="m-auto grid grid-cols-4 px-3 my-5 ">
+            <div className="col-span-1">
+              <div className="phone-number w-full h-full flex justify-start gap-3 items-center">
+                <div className="svg text-color">
+                  <FaPhoneAlt size={14} />
+                </div>
+                <div className="number text-color">09379424323</div>
+              </div>
+            </div>
+            <div className="col-span-2">
+              <div className="logo w-full h-full flex justify-center items-center ">
+                <span className="text-black font-bold text-2xl">salsal</span>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className="icon w-full h-full flex justify-end items-center">
+                <RxHamburgerMenu
+                  size={24}
+                  className="text-black cursor-pointer"
+                  onClick={(e) => {
+                    setIsShowMenu(true);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* {IsShowMenu && <div className="text-black">IsShowMenu</div>} */}
 
       <div className="mobile-header-none-scrolling grid grid-cols-4 px-3 mx-3 bg-white "></div>
 
--- app/components/theme/Home/MainShow.tsx
+++ app/components/theme/Home/MainShow.tsx
@@ -1,143 +1,140 @@
-
 export default function MainShow() {
   return (
-   <div className="Mainshow">
-    <div className="box p-12 mb-3 mx-3 grid grid-cols-12">
-      <div className="col-span-3">
-        <div className="detels-boxs-left py-12 h-full w-full flex flex-col justify-around items-center">
-          <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
+    <div className="Mainshow">
+      <div className="box p-12 mb-3 mx-3 grid grid-cols-12">
+        <div className="col-span-3">
+          <div className="detels-boxs-left py-12 h-full w-full flex flex-col justify-around items-center">
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+          </div>
         </div>
-         <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
+        <div className="main-box py-12 px-3 col-span-6 ">
+          <h2 className="bg-white rounded-full text-2xl max-w-xl mx-auto  text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx aspect-square">
+            انقلاب دیجیتال
+          </h2>
         </div>
-         <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
-        </div>
+        <div className="col-span-3">
+          <div className="detels-boxs-left py-12 h-full w-full flex flex-col justify-around items-center">
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+            <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx ">
+              بازاریابی
+            </div>
+          </div>
         </div>
       </div>
-      <div className="main-box py-12 col-span-6">
-        <h2 className="bg-white rounded-full text-2xl max-w-xl h-144 mx-auto  text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">انقلاب دیجیتال</h2>
-      </div>
-      <div className="col-span-3">
-        <div className="detels-boxs-left py-12 h-full w-full flex flex-col justify-around items-center">
-          <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
-        </div>
-         <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
-        </div>
-         <div className="details-box bg-white rounded-full text-2xl mx-auto w-full h-auto p-6 text-center flex justify-center items-center text-gray-400 border-[0.51px] border-gray-200 shadowbx">
-          بازاریابی
-        </div>
-        </div>
-      </div>
+
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 ">بنتذسیبتذنتیسبت</div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
+      <div className="box p-12 mb-3 mx-3 "></div>
     </div>
-
-
-
-
-
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 ">بنتذسیبتذنتیسبت</div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-    <div className="box p-12 mb-3 mx-3 "></div>
-   </div>
   );
 }
